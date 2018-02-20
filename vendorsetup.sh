20d734c7853797d9e7ed766df9a6eefa7d6e83c4
Christian Oder <myself5@carbonrom.org>
2018-02-20 23:26:49 +0100

    berkeley: Add CarbonROM support

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -14,18 +14,4 @@
 # limitations under the License.
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/treble_common.mk)
-
-BOARD_BUILD_DISABLED_VBMETAIMAGE := false
-
-# Inherit from berkeley device
-$(call inherit-product, device/huawei/berkeley/device.mk)
-
-# Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := berkeley
-PRODUCT_NAME := full_berkeley
-PRODUCT_BRAND := Huawei
-PRODUCT_MODEL := berkeley
+add_lunch_combo carbon_berkeley-userdebug
